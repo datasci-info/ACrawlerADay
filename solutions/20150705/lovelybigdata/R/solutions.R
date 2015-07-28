da738942ce89a037f09c67b0840fee954e27ae45
Mr Lai <excel2power@live.com>
2015-07-29 01:20:24 +0800

    add 20150705 only step1---3

--- solutions/20150705/lovelybigdata/R/solutions.R
+++ solutions/20150705/lovelybigdata/R/solutions.R
@@ -9,6 +9,8 @@
 #config
 #GET
 
+#### 沒把題意看清楚，只需要捉發文的時間就可以了
+
 library(httr) # 捉網頁用
 library(stringr) # 可使用正規表格法
 library(dplyr)  # 整理資料 提供 sql-like 語法
@@ -16,7 +18,6 @@ library(dplyr)  # 整理資料 提供 sql-like 語法
 # 求解順序如下
 # step1 單頁資料 (只找徵男): 
 #        (1) table_girl_new_article_info : 發文id, 發文時間
-#        (2) table_girl_res : 回應id(發文者親自回應), 回應時間
 # step2 多頁資料
 # step3 轉存SQLite:將上述資料轉存
 
@@ -42,8 +43,6 @@ girl_id = str_extract(girl_id_nickname, "\\w+")
 girl_id_is = str_detect( article_meta_value[3]  , '徵男') 
 
 # 確定發文者是女性時才執行以下動作，如果發文者是男性，也不用去管男性的回應如何……
-
-
 if (girl_id_is)  
   
 { 
@@ -51,24 +50,8 @@ if (girl_id_is)
   table_girl_new_article_info = c( girl_id , article_meta_value[4] )
   names(table_girl_new_article_info ) = c("girl_id" , "post_time") 
   
-  #step1 (2) table_girl_res
-  
-  #捉出使用者的時間
-  push_id = xpathSApply(res, '//*[@class="f3 hl push-userid"]', xmlValue)
-  
-  #捉出使用者回文的ip與時間
-  push_ip_time = xpathSApply(res, '//*[@class="push-ipdatetime"]', xmlValue)
-  
-  #使用正規表示法捉出時間 , 如果要找字串符合的起始位置，可以改用 str_locate
-  push_time = str_extract(push_ip_time, "[0-9][0-9]/[0-9][0-9]\\s[0-9][0-9]:[0-9][0-9]")   
-  
-  #轉成 data frame
-  table_girl_res= data.frame(  as.data.frame(push_id)  , as.data.frame(push_time)    )
-  
-  # 濾出發文者(女性)回應的時間
-  table_girl_res <- filter(table_girl_res , push_id == girl_id ) 
   
 }
 
 
-## Step2 .... 待補
+# step2 待補
