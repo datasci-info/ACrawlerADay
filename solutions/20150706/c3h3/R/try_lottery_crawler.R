91424e0b35488fcbd5d84ea170f3c26381055fab
c3h3 <c3h3.tw@gmail.com>
2015-08-01 16:57:25 +0800

    histogram of lottery over all history

--- solutions/20150706/c3h3/R/try_lottery_crawler.R
+++ solutions/20150706/c3h3/R/try_lottery_crawler.R
@@ -1,5 +1,5 @@
 library(XML)
-tables <- readHTMLTable("http://www.nfd.com.tw/lottery/49-year/49-2015.htm")
+tables <- readHTMLTable("http://www.nfd.com.tw/lottery/49-year/49-2015.htm",  stringsAsFactors = F )
 dataTable <- tables[[1]]
 View(dataTable)
 
@@ -7,3 +7,15 @@ colnames(dataTable) <- c("year","monthDay","YN","x1","x2","x3","x4","x5","x6","s
 
 df = dataTable[,c("x1","x2","x3","x4","x5","x6","s")]
 df
+
+library(dplyr)
+vec = do.call(c,as.list(df))
+names(vec) = NULL
+df = data.frame(x=vec)
+ndf = df %>% group_by(x) %>% summarise(n=n())
+barplot(ndf$n,names=ndf$x)
+
+ndf = df %>% group_by(x) %>% summarise(n=n()) %>% arrange(desc(n))
+
+barplot(ndf$n,names=ndf$x)
+head(ndf,10)
